8e8ae86d56acd73ac5a583faa34865db9868c543
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2022-02-09 10:34:53 -0600

    auth middleware

--- routes/blog.php
+++ routes/blog.php
@@ -3,7 +3,7 @@
 use Bolsainmobiliariape\ModuleBlog\Http\Controllers\Dashboard\Blog\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::as('dashboard.blog.')->middleware(['web'])->prefix('/dashboard/blog')->group(function (){
+Route::as('dashboard.blog.')->middleware(['web', 'auth'])->prefix('/dashboard/blog')->group(function (){
     Route::get('/', [IndexController::class, 'index'])->name('index');
     Route::get('/create', [IndexController::class, 'create'])->name('create');
     Route::get('/edit/{blog}', [IndexController::class, 'edit'])->name('edit');
